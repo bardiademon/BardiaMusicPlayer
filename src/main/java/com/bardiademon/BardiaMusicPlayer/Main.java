12f44dbc9a9710e9eaf1ce93bd72c08f6e5f5998
bardiademon <bardiademon@gmail.com>
2021-08-06 09:03:42 +0430

    First commit

---
+++ src/main/java/com/bardiademon/BardiaMusicPlayer/Main.java
@@ -0,0 +1,28 @@
+package com.bardiademon.BardiaMusicPlayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application
+{
+
+    @Override
+    public void start (Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load (Objects.requireNonNull (getClass ().getResource ("../../../../resources/sample.fxml")));
+        primaryStage.setTitle ("Hello World");
+        primaryStage.setScene (new Scene (root , 300 , 275));
+        primaryStage.show ();
+    }
+
+
+    public static void main (String[] args)
+    {
+        launch (args);
+    }
+}
---
+++ src/main/java/com/bardiademon/BardiaMusicPlayer/controllers/Controller.java
@@ -0,0 +1,4 @@
+package com.bardiademon.BardiaMusicPlayer.controllers;
+
+public class Controller {
+}
